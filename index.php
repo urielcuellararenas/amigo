641a3999a7fabc9c35b418f8ddb44e66862e1dd3
urielcuellararenas <uriel-rl@hotmail.com>
2019-12-03 09:51:34 -0500

    index

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Las 3 metaetiquetas anteriores * deben * ser lo primero en la cabeza; cualquier otro contenido principal debe venir * después * de estas etiquetas -->
+  <title>PLANTILLA | WEB 4</title>
+
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/animate.css">
+  <link href="css/animate.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet" />
+
+<body>
+
+  <div class="gallery">
+    <div class="text-center">
+      <h2>AMIGO SECRETO 2019</h2>
+    </div>
+    <div class="container">
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/8.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4><FONT COLOR="navy">GLORITA </FONT></h4>
+            <p>Clic para conocer tu amigo secreto.</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/9.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4><FONT COLOR="navy">BIBI</FONT></h4>
+            <p>Clic para conocer tu amigo secreto.</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/10.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4><FONT COLOR="navy">LILI </FONT></h4>
+            <p>Clic para conocer tu amigo secreto.</p>
+          </figcaption>
+        </figure>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/11.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4><FONT COLOR="navy">SERGIO </FONT></h4>
+            <p>Clic para conocer tu amigo secreto.</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/12.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4><FONT COLOR="navy">RODRI </FONT></h4>
+            <p>Clic para conocer tu amigo secreto.</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/13.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4><FONT COLOR="navy">ROCHI </FONT></h4>
+            <p>Clic para conocer tu amigo secreto.</p>
+          </figcaption>
+        </figure>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="js/jquery.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/wow.min.js"></script>
+  <script>
+    wow = new WOW({}).init();
+  </script>
+</body>
+
+</html>
+<style type="text/css">
+                
+              
+ h4
+{
+    font-weight: bolder;
+}
+
+</style>
